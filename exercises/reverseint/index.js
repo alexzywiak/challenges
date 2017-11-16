d8503303406d84672669fef70202a05e8ad50ac5
Alex Zywiak <alex.zywiak@gmail.com>
2017-11-16 15:42:55 -0700

    reverse int

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,13 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reverse = String(Math.abs(n))
+        .split('')
+        .reverse()
+        .join('');
+
+    return n >= 0 ? Number(reverse) : -1 * Number(reverse);
+}
 
 module.exports = reverseInt;
