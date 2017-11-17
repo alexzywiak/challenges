84a88fc5cf063741925c0ecca3f2a3633f8746c7
Alex Zywiak <alex.zywiak@gmail.com>
2017-11-17 09:41:55 -0700

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,38 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function createCharMap(str) {
+//     const map = {};
+
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//         map[char] = map[char] + 1 || 1;
+//     }
+
+//     return map;
+// }
+
+// function anagrams(stringA, stringB) {
+//     const aMap = createCharMap(stringA);
+//     const bMap = createCharMap(stringB);
+
+//     if (Object.keys(aMap).length !== Object.keys(bMap).length) {
+//         return false;
+//     }
+
+//     for (let char in aMap) {
+//         if (aMap[char] !== bMap[char]) {
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
+function anagrams(stringA, stringB) {
+    const sortA = stringA.replace(/[^\w]/, '').toLowerCase().split('').sort().join('');
+    const sortB = stringB.replace(/[^\w]/, '').toLowerCase().split('').sort().join('');
+
+    return sortA === sortB;
+}
 
 module.exports = anagrams;
